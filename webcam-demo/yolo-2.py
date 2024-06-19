0d61d261eab51b9439a9c1f1ff2b884f390a20ee
MidnightChaos <aidan.yung@gmail.com>
2024-06-19 16:35:45 -0700

    reformatted

--- webcam-demo/yolo-2.py
+++ webcam-demo/yolo-2.py
@@ -14,16 +14,88 @@ cap.set(4, 720)
 
 model = YOLO("../yolo-weights/yolov8n.pt")
 
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"]
+classNames = [
+    "person",
+    "bicycle",
+    "car",
+    "motorbike",
+    "aeroplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "backpack",
+    "umbrella",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis",
+    "snowboard",
+    "sports ball",
+    "kite",
+    "baseball bat",
+    "baseball glove",
+    "skateboard",
+    "surfboard",
+    "tennis racket",
+    "bottle",
+    "wine glass",
+    "cup",
+    "fork",
+    "knife",
+    "spoon",
+    "bowl",
+    "banana",
+    "apple",
+    "sandwich",
+    "orange",
+    "broccoli",
+    "carrot",
+    "hot dog",
+    "pizza",
+    "donut",
+    "cake",
+    "chair",
+    "sofa",
+    "pottedplant",
+    "bed",
+    "diningtable",
+    "toilet",
+    "tvmonitor",
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "microwave",
+    "oven",
+    "toaster",
+    "sink",
+    "refrigerator",
+    "book",
+    "clock",
+    "vase",
+    "scissors",
+    "teddy bear",
+    "hair drier",
+    "toothbrush",
+]
 
 while True:
     success, img = cap.read()
@@ -46,18 +118,19 @@ while True:
             # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 2)
             cvzone.cornerRect(img, (x1a, y1a, w, h))
 
-            #Confidence
+            # Confidence
             confidence = math.ceil((box.conf[0] * 100)) / 100
             print(confidence)
 
-
-            #Class
+            # Class
             cls = box.cls[0]
             print(cls)
 
-
             cvzone.putTextRect(
-                img, f"{classNames[int(cls)]} {confidence}", (x1a, y1a - 20), (max(0, x1a), max(35, y1a))
+                img,
+                f"{classNames[int(cls)]} {confidence}",
+                (x1a, y1a - 20),
+                (max(0, x1a), max(35, y1a)),
             )
 
     cv2.imshow("Image", img)
