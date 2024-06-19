374e2e8b95d7c9a0c2ba5364d175cf79af1083c7
MidnightChaos <aidan.yung@gmail.com>
2024-06-19 15:48:59 -0700

    finish testing

--- test/yolo-1.py
+++ test/yolo-1.py
@@ -2,13 +2,13 @@ import cv2
 from ultralytics import YOLO
 
 #import v8 nano
-model = YOLO('yolov8n.pt')
+model = YOLO('../yolo-weights/yolov8l.pt')
 
 #Test run
 results = model("images-test/image1.png", show=True)
 
-cv2.waitKey()
+cv2.waitKey(0)
 
 results = model("images-test/image2.png", show=True)
 
-cv2.waitKey()
+cv2.waitKey(0)
